87572272736e991679307668c54b1a759101c929
Sava-thecoolone <saveliyakov0401@gmail.com>
2025-04-24 22:28:44 +0400

    Update dyn_test.asm

--- assembler/examples/dyn_test.asm
+++ assembler/examples/dyn_test.asm
@@ -2,8 +2,8 @@
 ; writes value from B to screen position specified by A
 
 ; initialize registers
-MOV #1 -> A   ; screen position (e.g., column 10)
-MOV #10 -> B  ; value to write to screen
+MOV 1 -> A   ; screen position (e.g., column 10)
+MOV 10 -> B  ; value to write to screen
 
 ; write A to screen at position B
 MOV B -> %[A]  ; dynamic addressing - uses B as value
